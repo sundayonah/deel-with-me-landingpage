2b6dfbac27ba160d103554409f664d6a9140a1b5
Onah Sunday <sundayonah94@gmail.com>
2024-06-26 10:19:25 +0100

    Update HeroPage.tsx
    update landing page text

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -53,7 +53,7 @@ const HeroPage = () => {
         <div className='md:w-1/2'>
           <h1 className='text-base md:text-sm text-[#e58673] font-serif pb-2'>INSPIRING PEOPLE TO LOVE AND SHARE</h1>
           <span className='font-extrabold text-2xl md:text-5xl text-[#14183E]'>Over 10,000+ <br /> <span className='text-green-400'>Dealers </span> <span>ready to <br /> receive what you have got</span></span>
-          <p className='text-xs pt-4 text-[#5E6282]'>Welcome to Deel™ We are a grassroots & entirely <br /> nonprofit movement of people who are giving and <br /> getting stuff for free in their own Towns.</p>
+          <p className='text-xs pt-4 text-[#5E6282]'> We are a social enterprise, poised to make profit by giving true  <br />value to our altruistic users , therefore we make profit optimally  <br /> with a clear vision towards a greener and circle economy.</p>
           <div className='mt-4'>
             <button className='text-white text-xs my-2 px-4 py-2 rounded-md bg-[#f1a501]'>
               Sign Up
